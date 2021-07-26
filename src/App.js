80cd49ecedd79627456d0fce65263a649c8c9f89
shubham9gupta19 <shubham9gupta19@gmail.com>
2021-07-26 14:12:42 +0530

    Homepage to small components and Finshed styling menu-item

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Homepage from "./components/Homepage/Homepage.component";
+import Homepage from "./components/homepage/homepage.component";
 
 function App() {
   return (
--- src/components/Homepage/Homepage.component.jsx
+++ src/components/Homepage/Homepage.component.jsx
@@ -1,40 +1,10 @@
 import React from "react";
-import "./Homepage.styles.scss";
+import "./homepage.styles.scss";
+import Directory from "./../common/directory/directory.component";
 function Homepage(props) {
   return (
     <div className="homepage">
-      <div className="directory-menu">
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">HATS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">JACKETS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">SNEAKERS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">WOMENS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-        <div className="menu-item">
-          <div className="content">
-            <h1 className="title">MENS</h1>
-            <span className="subtitle">SHOP NOW</span>
-          </div>
-        </div>
-      </div>
+      <Directory />
     </div>
   );
 }
