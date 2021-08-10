0ee995a4418ea284c74601294b17c0eae46d1e4e
shubham9gupta19 <shubham9gupta19@gmail.com>
2021-08-10 17:53:27 +0530

    Hooks in shop,sign-in,sign-up Added

--- src/redux/cart/cart.sagas.js
+++ src/redux/cart/cart.sagas.js
@@ -1,6 +1,6 @@
 import { all, call, takeLatest, put } from "redux-saga/effects";
 
-import UserActionTypes from "../user/user.types";
+import { UserActionTypes } from "../user/user.types";
 import { clearCart } from "./cart.actions";
 
 export function* clearCartOnSignOut() {
